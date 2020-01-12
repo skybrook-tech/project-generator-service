51755c8764510f90cd495e778cece6ee7b72488a
Kito Clark <kclark@bridj.com>
2020-01-12 16:39:05 +1000

    added types

--- src/db/models/db.types.ts
+++ src/db/models/db.types.ts
@@ -3,6 +3,8 @@ import { __testModelRelated__ModelStatic } from "./__testModelRelated__/types";
 import { __testModel__ModelStatic } from "./__testModel__/types";
 
 export interface Db {
+  sequelize: Sequelize;
+  Sequelize: any;
   __testModelRelated__: __testModelRelated__ModelStatic;
   __testModel__: __testModel__ModelStatic;
 }
